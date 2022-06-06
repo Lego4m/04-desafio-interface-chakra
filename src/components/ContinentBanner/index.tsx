35bfb9a038253b2884a187f85ab69281eca93539
Leonardo Maron <leo22.maron@hotmail.com>
2022-06-06 13:03:05 -0300

    ContinentsBanner

---
+++ src/components/ContinentBanner/index.tsx
@@ -0,0 +1,26 @@
+import { Flex, Heading } from '@chakra-ui/react';
+
+export function ContinentBanner() {
+  return (
+    <Flex
+      w='100%'
+      maxW={1440}
+      h='500px'
+      mx='auto'
+      bgRepeat='no-repeat'
+      backgroundImage='/ContinentsImgs/europe.png'
+      align='flex-end'
+      pl='36'
+      pb='14'
+    >
+      <Heading
+        textAlign='left'
+        fontWeight='semibold'
+        fontSize='5xl'
+        lineHeight='4.5rem'
+      >
+        Europa
+      </Heading>
+    </Flex>
+  )
+}
